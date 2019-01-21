8c5c7cf44f77e059c228e4669ae7e52264f24edf
itsourav <it.sourav@gmail.com>
2019-01-21 06:35:25 -0500

    duplicate files deleted

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    FB_PAGE_TOKEN: "EAAbSiuhJ9rsBALZBZAS5xfw6vGT9o43WDyU2BlmhGoAmjTrm8zamZAapuz1ZBtyBiCET7lPFJXHupxQDiNQy5VLbSX77bRrvXOPg9iN8i2hKn7L00FRShEZByWxaEaPAcr8K8iTc5v60sWZArtTTZBEGbC8Tqce1WlN2p58j9WfnQZDZD",
-    FB_VERIFY_TOKEN: "sourav_facebook",
-    API_AI_CLIENT_ACCESS_TOKEN: "a0c07b6e59d74fcdbc9ca687b03b6ec8",
-    FB_APP_SECRET: "Facebook Secret Code",
-  };
